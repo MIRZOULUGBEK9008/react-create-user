a5983449488653e86b105ceea1dca3ae3ade8cec
Mirzo Ulug'bek Xudoyberdiyev <ulugbekkhudoyberdiyev9008@gmail.com>
2023-10-09 10:15:57 +0500

    Add items and rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import App from "./App.jsx";
 import "./css/normalize.css";
 import "./css/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
